1a2ce1f689472e793e475a48a4d110b1d9a728f8
Kenneth Lyons <ixjlyons@gmail.com>
2022-07-18 16:27:54 -0700

    Formatting, add om version

--- sellar.py
+++ sellar.py
@@ -1,6 +1,5 @@
 import aesara
 import aesara.tensor as at
-from aesara.compile import io
 from scipy import optimize
 
 xx = at.vector("xx")
@@ -10,10 +9,9 @@ z2 = xx[2]
 y1_in = xx[3]
 y2_in = xx[4]
 
-
-y1_out = z1 ** 2 + z2 + x - 0.2 * y2_in
+y1_out = z1**2 + z2 + x - 0.2 * y2_in
 y2_out = at.sqrt(y1_in) + z1 + z2
-obj = x ** 2 + z2 + y1_in + at.exp(-y2_in)
+obj = x**2 + z2 + y1_in + at.exp(-y2_in)
 
 con1 = y1_in - 3.16
 con2 = 24.0 - y2_out
@@ -24,21 +22,18 @@ Ins = [xx]
 
 f_obj = aesara.function(Ins, obj)
 f_con1 = aesara.function(Ins, con1)
-f_con2 = aesara.function(Ins, con2,)
-f_con_y1 = aesara.function(Ins, con_y1,)
-f_con_y2 = aesara.function(Ins, con_y2,)
+f_con2 = aesara.function(Ins, con2)
+f_con_y1 = aesara.function(Ins, con_y1)
+f_con_y2 = aesara.function(Ins, con_y2)
 
-d_obj = aesara.function(Ins,    at.grad(obj, xx))
-d_con1 = aesara.function(Ins,   at.grad(con1, xx))
-d_con2 = aesara.function(Ins,   at.grad(con2, xx))
+d_obj = aesara.function(Ins, at.grad(obj, xx))
+d_con1 = aesara.function(Ins, at.grad(con1, xx))
+d_con2 = aesara.function(Ins, at.grad(con2, xx))
 d_con_y1 = aesara.function(Ins, at.grad(con_y1, xx))
 d_con_y2 = aesara.function(Ins, at.grad(con_y2, xx))
 
-f_y1 = aesara.function(Ins, y1_out,)
-f_y2 = aesara.function(Ins, y2_out,)
-# problem:
-# minimize obj wrt x1, z1, z2
-# subject to con1 <= 0, con2 <= 0, 0 < x < 10, 0 < z1 < 10, 0 < z2 < 10
+f_y1 = aesara.function(Ins, y1_out)
+f_y2 = aesara.function(Ins, y2_out)
 
 res = optimize.minimize(
     f_obj,
@@ -57,7 +52,7 @@ res = optimize.minimize(
 print(res)
 
 print("checking...")
-print("y1 =", f_y1(res.x),)
-print("3.16 <= y1?", 3.16 <= res.x[3] )
-print("y2 =", f_y2(res.x),)
-print("y2 <= 24.0?", res.x[4] <= 24.0 )
+print("y1 =", f_y1(res.x))
+print("3.16 <= y1?", 3.16 <= res.x[3])
+print("y2 =", f_y2(res.x))
+print("y2 <= 24.0?", res.x[4] <= 24.0)
