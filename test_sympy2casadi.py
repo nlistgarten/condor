7efdbcb56d3970a155b0ba1455504762af9b6eb4
Kenneth Lyons <kenneth.r.lyons@nasa.gov>
2023-02-17 14:15:47 -0800

    Add sympy2casadi test

---
+++ test_sympy2casadi.py
@@ -0,0 +1,49 @@
+import upcycle  # isort: skip
+import casadi
+import sympy as sym
+from sympy.printing.pycode import PythonCodePrinter
+
+class CustomPrinter(PythonCodePrinter):
+    def _print_Piecewise(self, expr):
+        eargs = expr.args
+
+        def recurse(eargs):
+            val, cond = eargs[0]
+            if len(eargs) == 1:
+                return str(val)
+            else:
+                rest = eargs[1:]
+                return f"if_else({cond}, {val}, {recurse(rest)})"
+
+        return recurse(eargs)
+
+    def _print_Pow(self, expr, rational=False):
+        return self._hprint_Pow(expr, rational=rational, sqrt="casadi.sqrt")
+
+# sym_prob, res_mat, out_syms = upcycle.upcycle_problem(up_prob)
+x1, x2 = sympy_var = sym.symbols("x1:3")
+sympy_expr = sym.Matrix(
+    [
+        sym.sqrt(x1 * x2),
+        # sym.Piecewise((x1, x1 > 1), (x2, x1 < -1), (x1 + x2, True)),
+    ],
+)
+
+casadi_var_in = casadi.vertcat(*[casadi.MX.sym(s.name) for s in sympy_var])
+# ca_res, ca_vars_out = upcycle.sympy2casadi(exprs, syms, ca_vars)
+
+if casadi_var_in.shape[1] > 1:
+    casadi_var_in = casadi_var_in.T
+casadi_var = casadi.vertsplit(casadi_var_in)
+
+mapping = {
+    "ImmutableDenseMatrix": casadi.blockcat,
+    "MutableDenseMatrix": casadi.blockcat,
+    "Abs": casadi.fabs,
+    "Array": casadi.MX,
+}
+
+f = sym.lambdify(sympy_var, sympy_expr, modules=[mapping, casadi])#, printer=CustomPrinter)
+
+out = f(*casadi_var)
+cf = casadi.Function("f", casadi.vertsplit(casadi_var_in), casadi.vertsplit(out))
