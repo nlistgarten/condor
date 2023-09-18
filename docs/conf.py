4f635c94274fcac60577fba40601224b74bbf3bd
Kenneth Lyons <kenneth.r.lyons@nasa.gov>
2023-09-18 10:59:14 -0700

    Add version to docs

--- docs/conf.py
+++ docs/conf.py
@@ -6,16 +6,20 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import condor
+
 project = 'Condor'
 copyright = '2023, Benjamin W. L. Margolis'
 author = 'Benjamin W. L. Margolis'
+version = condor.__version__
+release = version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+# templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
@@ -24,4 +28,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['_static']
+# html_static_path = ['_static']
