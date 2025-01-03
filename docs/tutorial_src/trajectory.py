9a82bd984571e61249dacb33c176203b1bd6df42
Benjamin Margolis <benjamin.margolis@nasa.gov>
2025-01-03 12:00:00 -0800

    tweaks for trajectory

--- docs/tutorial_src/trajectory.py
+++ docs/tutorial_src/trajectory.py
@@ -9,7 +9,8 @@ Working with trajectories
 # dynamical systems nice.
 #
 # For this tutorial, we will consider a simplified model of a glider with some form of
-# angle-of-attack control.
+# angle-of-attack control. We can represent this as a system of ordinary differential
+# equations (ODEs) given by
 #
 # .. math::
 #    \begin{align}
@@ -22,8 +23,8 @@ Working with trajectories
 # where :math:`r` is the range, or horizontal position, :math:`h` is the altitude, or
 # vertical position, :math:`v` is the velocity,  :math:`\gamma` is the flight-path
 # angle, and :math:`\alpha` is the angle-of-attack, which modulates the coefficients of
-# lift, :math:`CL`, and drag, :math:`CD`.  Simple models of the lift and drag are given
-# by
+# lift, :math:`CL`, and drag, :math:`CD`, and :math:`g` is the acceleration due to
+# gravity. Simple models of the lift and drag are given by
 #
 # .. math::
 #    \begin{align}
@@ -53,10 +54,16 @@ class Glider(condor.ODESystem):
     CL = CL_alpha * alpha
     CD = CD_0 + CD_i_q * CL**2
 
+
     dot[r] = v * ops.cos(gamma)
     dot[h] = v * ops.sin(gamma)
     dot[gamma] = (CL * v**2 - g*ops.cos(gamma) )/v
     dot[v] = -CD * v**2 - g*ops.sin(gamma)
+
+    initial[r] = 0.
+    initial[h] = 1.
+    initial[v] = 15.
+    initial[gamma] = 30*ops.pi/180.
 
 
 # %%
@@ -64,23 +71,22 @@ class Glider(condor.ODESystem):
 # behavior, so we use this for the angle-of-attack so we can simulate multiple
 # behaviors. To simulate this model, we create a :class:`TrajectoryAnalysis`, a sub-model
 # to an ODE Sysstem, which is ultimately responsible for defining the specifics of
-# integrating the ODE. In this example, we will fix the initial conditions and final
-# simulation time of the model.
+# integrating the ODE. In this example, the :class:`TrajectoryAnalysis` model only
+# specifies the final simulation time of the model.  It is more mathematically
+# consistent to have the initial values defined in the trajectory analysis, but for
+# convience we declared it as part of the ODE system.
 
 class FirstSim(Glider.TrajectoryAnalysis):
-    initial[r] = 0.
-    initial[h] = 1.
-    initial[v] = 15.
-    initial[gamma] = 30*ops.pi/180.
     tf = 20.
 
 # %%
 # The fields of the original :class:`Glider` simulation are copied to the
 # :class:`TrajectoryAnalysis` so the parameter values must be supplied to evaluate the
 # model numerically.
+#
 
 A = 3E-1
-first_sim = FirstSim(CL_alpha = 2*ops.pi*A, CD_0 = 0.05*A, CD_i_q = 0.05, g=1.)
+first_sim = FirstSim(CL_alpha = 0.11*A, CD_0 = 0.05*A, CD_i_q = 0.05, g=1.)
 
 # %%
 # In addition to binding static parameters like the other built-in models, the
@@ -110,12 +116,12 @@ time_history_plot([first_sim])
 # %%
 # and the flight path
 
-def flight_path_plot(sims):
+def flight_path_plot(sims, line_spec="o-"):
     fig, ax = plt.subplots(constrained_layout=True)
     plt.ylabel("altitude")
     plt.xlabel("range")
     for idx, sim in enumerate(sims):
-        ax.plot(sim.r, sim.h, f'C{len(sims)-idx-1}o-')
+        ax.plot(sim.r, sim.h, f'C{len(sims)-idx-1}{line_spec}')
     plt.grid(True)
     ax.set_aspect('equal')
     plt.ylim(0., 30)
@@ -134,30 +140,41 @@ class Bounce(Glider.Event):
 
 # %%
 # but we still need to create a new :class:`TrajectoryAnalysis` since the
-# :class:`FirstSim` was bound to the :class:`Glider` model at the time of creation.
+# :class:`FirstSim` was bound to the :class:`Glider` model at the time of creation
+# (without the bounce event).
 
 class BounceSim(Glider.TrajectoryAnalysis):
-    initial[r] = 0.
-    initial[h] = 1.
-    initial[v] = 15.
-    initial[gamma] = 30*ops.pi/180.
     tf = 20.
 
 bounce_sim = BounceSim(**first_sim.parameter.asdict(), mu=0.9)
 
 flight_path_plot([bounce_sim, first_sim])
+plt.legend(["with bounce", "original sim"])
 
 # %%
-# and we can also add a behavior for the angle of attack using a mode, in this case
+# We can also add a behavior for the angle of attack using a mode, in this case
 # holding a constant half-degree angle of attack after reaching peak altitude to reduce
-# rate of descent.
-
-class AlphaAttack(Glider.Mode):
-    condition = gamma < 0
-    action[alpha] = 0.5*ops.pi/180.
+# rate of descent. To ensure proper numerical behavior, we follow [orbital ref] and use 
+# of an accumulator state to encode the flight controller logic. In this case we track
+# the range at the start of descent, and use that as a trigger for holding a non-zero
+# angle of attack.
 
 class MaxAlt(Glider.Event):
     function = gamma
+    #accum_state = state(name="range_at_max_alt")
+    #initial[accum_state] = 1E3
+    #update[accum_state] = r * (dot[r] > 0)
+    accum_state = state(name="max_alt")
+    initial[accum_state] = 0
+    update[accum_state] = h * (h > accum_state) + accum_state * (h <= accum_state)
+
+
+class AlphaAttack(Glider.Mode):
+    condition = gamma < 0
+    condition = (r >= MaxAlt.accum_state)# * (gamma < 0)
+    condition = 1.
+    hold_alpha = parameter()
+    action[alpha] = hold_alpha
 
 class AlphaSim(Glider.TrajectoryAnalysis):
     initial[r] = 0.
@@ -166,9 +183,138 @@ class AlphaSim(Glider.TrajectoryAnalysis):
     initial[gamma] = 30*ops.pi/180.
     tf = 20.
 
-alpha_sim = AlphaSim(**bounce_sim.parameter.asdict())
+alpha_sim = AlphaSim(**bounce_sim.parameter.asdict(), hold_alpha = 0.5)
 
 flight_path_plot([alpha_sim, bounce_sim, first_sim])
+
+
+# %%
+# So far, we have only used the :class:`TrajectoryAnalysis` to simulate the ODE System.
+# In order to use the ODE system as part of other condor models, we must declare
+# :attr:`trajectory_output`. Condor computes the gradient of the
+# :attr:`trajectory_output` using the Sweeping Gradient method. Each trajectory output
+# has the form
+#
+# .. math::
+#    J = \phi\left(t_{f},x\left(t_{f}\right),p\right)+\int_{t_{0}}^{t_{f}}L\left(\tau,x\left(\tau\right),p\right)\,d\tau
+#
+# where :math:`\phi` is the terminal term, :math:`L\left(\cdot\right)` is the integrand
+# term, and :math:`x\left(t\right)` is the solution to the system of ODEs with events
+# and modes. We can form the area under the flight-path curve by taking the derivative
+# and using it to form the integrand.
+
+Bounce.terminate = True
+
+class AlphaSim(Glider.TrajectoryAnalysis):
+    initial[r] = 0.
+    initial[h] = 1.
+    initial[v] = 15.
+    initial[gamma] = 30*ops.pi/180.
+    tf = 100.
+
+    area = trajectory_output(integrand=dot[r]*h)
+    max_h = trajectory_output(MaxAlt.accum_state)
+    max_r = trajectory_output(r)
+    #area = trajectory_output(integrated_area)
+    #area = trajectory_output(r)
+    #area = trajectory_output(-t)
+    #area2 = trajectory_output(integrand=h)
+
+    class Options:
+        if True:
+            state_rtol = 1E-12
+            state_atol = 1E-15
+            adjoint_rtol = 1E-12
+            adjoint_atol = 1E-15
+        #state_solver = condor.backend.implementations.TrajectoryAnalysis.Solver.dop853
+
+
+
+
+
+# %%
+# Then we can compare areas,
+
+results = {
+    "alpha = +0.5 deg": AlphaSim(**bounce_sim.parameter.asdict(), hold_alpha = 0.5),
+    "alpha = 0.0 deg": AlphaSim(**bounce_sim.parameter.asdict(), hold_alpha = 0.0),
+    "alpha = -0.5 deg": AlphaSim(**bounce_sim.parameter.asdict(), hold_alpha = -0.5),
+}
+
+print(*[f"{k}: {v.area}" for k, v in results.items()])
+#print(*[f"{k}: {v.area2}" for k, v in results.items()])
+
+flight_path_plot(results.values())
+plt.legend([k.replace("alpha", r"$\alpha$") for k in results])
+
+
+# %%
+# With several :attr:`trajectory_output`, we can embed it within other Condor models,
+# for example to maximize a combination of the  area under the curve and maximum
+# altitude. We will also use the :attr:`iter_callback` to save each trajectory the
+# optimizer tries. This code block will declare the optimization problem, run it, and
+# plot each trajectory.
+
+class Callback:
+    def __init__(self):
+        self.parameter = None
+
+    @classmethod
+    def attach(cls, options):
+        options._callback = cls()
+        options.init_callback = options._callback.init_callback
+        options.iter_callback = options._callback.iter_callback
+
+    def init_callback(self, parameter, impl_opts):
+        self.parameter = parameter
+        print("starting optimization with...")
+        #print(impl_opts)
+        print(parameter)
+        self.sims = []
+
+    def iter_callback(self, i, variable, objective, constraint):
+        iter_opt_res = MaxArea.from_values(
+            **variable.asdict(),
+            **self.parameter.asdict(),
+        )
+        self.sims.append(iter_opt_res)
+        print(f"iter {i}: {variable.asdict()}, area={iter_opt_res.sim.area}")
+
+
+class MaxArea(condor.OptimizationProblem):
+    alpha = variable(
+        initializer=0.001,
+        lower_bound=-1.,
+        upper_bound=1,
+        warm_start=False,
+    )
+    sim = AlphaSim(**bounce_sim.parameter.asdict(), hold_alpha = alpha)
+    trade_off = parameter()
+    objective = -(trade_off*sim.area + (1-trade_off)*sim.max_h)
+    objective = -(trade_off*sim.area + (1-trade_off)*sim.max_r)
+    objective = -(trade_off*sim.max_h + (1-trade_off)*sim.max_r)
+
+    class Options:
+        exact_hessian = False
+        print_level = 0
+        tol = 1E-3
+        max_iter = 10
+
+Callback.attach(MaxArea.Options)
+for title, to in {
+        #"max altitude": 0.,
+        #"max area": 1.,
+        "max range": 0.,
+        "max altitude": 1.,
+}.items():
+    print("\n"*3, title)
+    opt = MaxArea(trade_off=to)
+    opt_sims = opt.Options._callback.sims
+    flight_path_plot([sim.sim for sim in opt_sims][::-1], '-')
+    plt.legend([f"iter {idx}, alpha={sim.alpha}" for idx, sim in enumerate(opt_sims)][::-1])
+    plt.title(f"optimization iterations for {title}")
+    plt.ylim(0, 45)
+    plt.xlim(0, 85)
 
 plt.show()
 
