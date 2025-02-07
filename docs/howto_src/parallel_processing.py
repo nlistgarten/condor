8bafff528f8ec340c8a8c797e7e961db4403c890
Kenneth Lyons <kenneth.r.lyons@nasa.gov>
2025-02-07 10:28:07 -0800

    Fix parallel processing howto

--- docs/howto_src/parallel_processing.py
+++ docs/howto_src/parallel_processing.py
@@ -1,30 +1,26 @@
-============================
+"""
+===================
 Parallel Processing
-============================
+===================
+"""
 
-You should be able to treat models like any other function in terms of parallelization.
-This example shows using the built-in :mod:`multiprocessing` to do thread-based
-parallelization of an explicit system.
+# %%
+# You should be able to treat models like any other function in terms of
+# parallelization.  This example shows using the built-in :mod:`multiprocessing` to do
+# process-based parallelization of an explicit system.
 
-.. code-block:: python
+from multiprocessing import Pool
 
-    import condor
-    class Model(condor.ExplicitSystem):
-        x = input()
-        output.y = -x**2 + 2*x + 1
-
-    from multiprocessing import Pool
-    if __name__ == "__main__":
-        with Pool(5) as p:
-            models = p.map(Model, [1, 2, 3])
-
-        for model in models:
-            print(model.input, model.output)
+import condor
 
 
-    # produces...
-    """
-    ModelInput(x=1) ModelOutput(y=array([2.]))
-    ModelInput(x=2) ModelOutput(y=array([1.]))
-    ModelInput(x=3) ModelOutput(y=array([-2.]))
-    """
+class Model(condor.ExplicitSystem):
+    x = input()
+    output.y = -x**2 + 2*x + 1
+
+
+with Pool(5) as p:
+    models = p.map(Model, [1, 2, 3])
+
+for model in models:
+    print(model.input, model.output)
