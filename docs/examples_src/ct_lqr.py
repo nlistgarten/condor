bec2191fa8cdba3613d127dc94887f0365245325
Kenneth Lyons <kenneth.r.lyons@nasa.gov>
2025-08-07 16:55:37 -0700

    Get LQR examples working without contrib.LTI

--- docs/examples_src/ct_lqr.py
+++ docs/examples_src/ct_lqr.py
@@ -3,30 +3,33 @@ Continuous Time LQR
 ===================
 """
 
-from time import perf_counter
-
 import matplotlib.pyplot as plt
 import numpy as np
 from _sgm_test_util import LTI_plot
-from scipy import linalg
 
 import condor as co
 
-dblintA = np.array([[0, 1], [0, 0]])
-dblintB = np.array([[0, 1]]).T
 
-DblInt = co.LTI(a=dblintA, b=dblintB, name="DblInt")
+class DblInt(co.ODESystem):
+    A = np.array([[0.0, 1.0], [0.0, 0.0]])
+    B = np.array([[0.0], [1.0]])
+
+    K = parameter(shape=(1, B.shape[0]))
+
+    x = state(shape=A.shape[0])
+    dynamic_output.u = -K @ x
+
+    dot[x] = A @ x + B @ u
 
 
 class DblIntLQR(DblInt.TrajectoryAnalysis):
+    tf = 32.0
+
     initial[x] = [1.0, 0.1]
 
     Q = np.eye(2)
     R = np.eye(1)
 
-    tf = 32.0
-
-    u = dynamic_output.u
     cost = trajectory_output(integrand=(x.T @ Q @ x + u.T @ R @ u) / 2)
 
     class Options:
@@ -34,36 +37,44 @@ class DblIntLQR(DblInt.TrajectoryAnalysis):
         adjoint_rtol = 1e-8
 
 
-ct_sim = DblIntLQR(k=[1.0, 0.1])
+ct_sim = DblIntLQR(K=[1.0, 0.1])
 LTI_plot(ct_sim)
 
 
 # %%
+# Determine the optimal gain by embedding the trajectory analysis in an optimization
+# problem:
 
 
 class CtOptLQR(co.OptimizationProblem):
-    k = variable(shape=DblIntLQR.k.shape)
-    objective = DblIntLQR(k).cost
+    K = variable(shape=DblIntLQR.K.shape)
+
+    sim = DblIntLQR(K=K)
+    objective = sim.cost
 
     class Options:
-        # exact_hessian = False
         __implementation__ = co.implementations.ScipyCG
 
 
-t_start = perf_counter()
 lqr_sol = CtOptLQR()
-t_stop = perf_counter()
 
+print(lqr_sol.K)
 
-S = linalg.solve_continuous_are(dblintA, dblintB, DblIntLQR.Q, DblIntLQR.R)
-k = linalg.solve(DblIntLQR.R, dblintB.T @ S)
+# %%
+# Compare with the solution from the continuous algebraic Riccati equation:
 
-lqr_are = DblIntLQR(k)
+from scipy import linalg
 
-print(lqr_sol._stats)
-print(lqr_are.cost, lqr_sol.objective)
-print(lqr_are.cost > lqr_sol.objective)
-print("      ARE sol:", k, "\niterative sol:", lqr_sol.k)
-print("time to run:", t_stop - t_start)
+S = linalg.solve_continuous_are(DblIntLQR.A, DblIntLQR.B, DblIntLQR.Q, DblIntLQR.R)
+K = linalg.solve(DblIntLQR.R, DblIntLQR.B.T @ S)
+
+print(K)
+
+# %%
+
+sim_are = DblIntLQR(K=K)
+LTI_plot(sim_are)
+
+# %%
 
 plt.show()
