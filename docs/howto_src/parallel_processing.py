27415cb6d0172f42684a2b162aa5ba0749a4b4f1
Kenneth Lyons <kenneth.r.lyons@nasa.gov>
2025-08-07 17:09:32 -0700

    Try using freeze_support for multiprocessing example

--- docs/howto_src/parallel_processing.py
+++ docs/howto_src/parallel_processing.py
@@ -6,10 +6,10 @@ Parallel Processing
 
 # %%
 # You should be able to treat models like any other function in terms of
-# parallelization.  This example shows using the built-in :mod:`multiprocessing` to do
+# parallelization. This example shows using the built-in :mod:`multiprocessing` to do
 # process-based parallelization of an explicit system.
 
-from multiprocessing import Pool
+import multiprocessing
 
 import condor
 
@@ -20,7 +20,9 @@ class Model(condor.ExplicitSystem):
 
 
 if __name__ == "__main__":
-    with Pool(5) as p:
+    multiprocessing.freeze_support()
+
+    with multiprocessing.Pool(5) as p:
         models = p.map(Model, [1, 2, 3])
 
     for model in models:
