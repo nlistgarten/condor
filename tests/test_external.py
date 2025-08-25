bc92852ed0dd6225fffc3534ff8108ad6195f17a
Benjamin Margolis <benjamin.margolis@nasa.gov>
2025-08-25 12:09:16 -0700

    failing test

--- tests/test_external.py
+++ tests/test_external.py
@@ -10,43 +10,105 @@ def test_external_solver_dict_io():
             self.input(name="a")
             self.input(name="b", shape=3)
             self.output(name="x", shape=3)
+            self.output(name="y", shape=1)
 
         def function(self, inputs):
             x = inputs["a"] ** 2 + 2 * inputs["b"] ** 2
-            return {"x": x}
+            y = ops.sin(inputs["a"])
+            return dict(x=x, y=y)
+
+        def jacobian(self, input):
+            dyda = ops.cos(input.a)
+            dxda = 2 * input.a * np.ones_like(input.b)
+            dxdb = 4 * np.diag(input.b.squeeze())
+            # out of order on purpose
+            return {
+                ("y", "a"): dyda,
+                # should you have to provide this?
+                # ("y", "b"): np.zeros((1,3)),
+                ("x", "b"): dxdb,
+                ("x", "a"): dxda,
+            }
+
+    class NumericLike(condor.ExternalSolverWrapper):
+        def __init__(self):
+            self.input(name="a")
+            self.input(name="b", shape=3)
+            self.output(name="x", shape=3)
+            self.output(name="y")
+
+        def function(self, inputs):
+            a, b = inputs.asdict().values()
+            x = a**2 + 2 * b**2
+            y = ops.sin(a)
+            return np.concat([x.squeeze(), np.atleast_1d(y)])
+
+            return x, y
+            out = np.array((4, 1))
+            out[:3, 0] = x.squeeze()
+            out[3, 0] = y
+            return out
 
         def jacobian(self, input):
             dxda = 2 * input.a * np.ones_like(input.b)
             dxdb = 4 * np.diag(input.b.squeeze())
+            dyda = ops.cos(input.a)
             # out of order on purpose
-            return {("x", "b"): dxdb, ("x", "a"): dxda}
+            jac = np.zeros((4, 4))
+            jac[:3, 0] = dxda.squeeze()
+            jac[:3, 1:] = dxdb
+            jac[3, 0] = dyda
+            return jac
 
-    class Analytic(condor.ExplicitSystem):
+    class Condoric(condor.ExplicitSystem):
         a = input()
         b = input(shape=3)
         output.x = a**2 + 2 * b**2
+        output.y = ops.sin(a)
+
+    dsys = DictLike()
+    nsys = NumericLike()
 
     class Jac(condor.ExplicitSystem):
-        sys = DictLike()
-        inp = input.create_from(sys.input)
-        num_out = sys(**inp)
+        inp = input.create_from(dsys.input)
+        dsys_out = dsys(**inp)
+        nsys_out = nsys(**inp)
+        c_out = Condoric(**inp)
 
-        anal_out = Analytic(**num_out.input)
+        output.dsys_dxda = ops.jacobian(dsys_out.x, input.a)
+        output.dsys_dxdb = ops.jacobian(dsys_out.x, input.b)
+        output.dsys_dyda = ops.jacobian(dsys_out.y, input.a)
+        output.dsys_dydb = ops.jacobian(dsys_out.y, input.b)
 
-        output.num_dxda = ops.jacobian(num_out.x, input.a)
-        output.num_dxdb = ops.jacobian(num_out.x, input.b)
+        output.nsys_dxda = ops.jacobian(nsys_out.x, input.a)
+        output.nsys_dxdb = ops.jacobian(nsys_out.x, input.b)
+        output.nsys_dyda = ops.jacobian(nsys_out.y, input.a)
+        output.nsys_dydb = ops.jacobian(nsys_out.y, input.b)
 
-        output.anal_dxda = ops.jacobian(anal_out.x, input.a)
-        output.anal_dxdb = ops.jacobian(anal_out.x, input.b)
+        output.c_dxda = ops.jacobian(c_out.x, input.a)
+        output.c_dxdb = ops.jacobian(c_out.x, input.b)
+        output.c_dyda = ops.jacobian(c_out.y, input.a)
+        output.c_dydb = ops.jacobian(c_out.y, input.b)
 
-    sys = DictLike()
-    # out = sys(a=2, b=np.array([[1, 2, 3], [4, 5, 6]]))
-    out = sys(a=2, b=np.array([[1, 2, 3]]))
-    assert np.all(out.x.squeeze() == (out.a**2 + 2 * out.b**2).squeeze())  # [1, 0]
+    dsys_out = dsys(a=2, b=np.array([[1, 2, 3]]))
+    nsys_out = nsys(**dsys_out.input)
 
-    out_jac = Jac(**out.input)
-    assert np.all(out_jac.anal_dxda == out_jac.num_dxda)
-    assert np.all(out_jac.anal_dxdb == out_jac.num_dxdb)
+    assert np.all(
+        dsys_out.x.squeeze() == (dsys_out.a**2 + 2 * dsys_out.b**2).squeeze()
+    )  # [1, 0]
+    assert np.all(dsys_out.y.squeeze() == np.sin(dsys_out.a).squeeze())  # [1, 0]
+
+    out_jac = Jac(**dsys_out.input)
+    assert np.all(out_jac.c_dxda == out_jac.dsys_dxda)
+    assert np.all(out_jac.c_dxdb == out_jac.dsys_dxdb)
+    assert np.all(out_jac.c_dyda == out_jac.dsys_dyda)
+    assert np.all(out_jac.c_dydb == out_jac.dsys_dydb)
+
+    assert np.all(out_jac.c_dxda == out_jac.nsys_dxda)
+    assert np.all(out_jac.c_dxdb == out_jac.nsys_dxdb)
+    assert np.all(out_jac.c_dyda == out_jac.nsys_dyda)
+    assert np.all(out_jac.c_dydb == out_jac.nsys_dydb)
+    breakpoint()
 
 
 def test_external_solver_array_io():
@@ -55,3 +117,7 @@ def test_external_solver_array_io():
             self.input(name="a")
             self.input(name="b", shape=(2, 3))
             self.output(name="x")
+
+
+if __name__ == "__main__":
+    test_external_solver_dict_io()
